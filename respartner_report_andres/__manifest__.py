001874615b78d57ae9bd71bb642d861a365b5104
ANDAGU2 <57407799+ANDAGU2@users.noreply.github.com>
2021-04-25 12:15:25 -0500

    respartner

---
+++ respartner_report_andres/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'respartner ADG',
+
+    'version': '13.1',
+
+    'author': "Andres Gutierrez",
+
+    'contributors': ['Andres David Gutierrez andresdavgut@hotmail.com'],
+
+    'website': "",
+
+    'category': 'Reports',
+
+    'depends': [
+
+       
+        'base',
+        
+
+    ],
+
+    'data': [
+       
+        'reports/respartner_report_andres.xml',
+       
+                  
+    ],
+    'installable': True
+}
